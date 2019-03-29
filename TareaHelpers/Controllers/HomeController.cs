be63111346e307cab532f163fa47fa738e09a656
ManuxGT <imccs05@hotmail.com>
2019-03-29 12:46:22 -0400

    Todo funcionando

--- TareaHelpers/Controllers/HomeController.cs
+++ TareaHelpers/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TareaHelpers.Models;
 
 namespace TareaHelpers.Controllers
 {
@@ -26,5 +27,83 @@ namespace TareaHelpers.Controllers
 
             return View();
         }
+
+        bool IsValidEmail(string email)
+        {
+            try
+            {
+                var addr = new System.Net.Mail.MailAddress(email);
+                return addr.Address == email;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public ActionResult Registrar()
+        {
+           
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Registrar(string nombre,string apellido,string cedula,int edad, string telefono,
+            string estado_civil, string correo,bool Videojuegos, string genero, bool Gimnasio, bool Leer, bool Musica)
+        {
+            
+            
+            Persona alguien = new Persona();
+            alguien.nombre = nombre;
+            alguien.apellido = apellido;
+            alguien.correo = correo;
+            alguien.cedula = cedula;
+            alguien.edad = edad;
+            alguien.genero = genero;
+            alguien.telefono = genero;
+            alguien.estado_civil = estado_civil;
+
+            if (Videojuegos)
+            {
+                string v = "Videojuegos";
+                alguien.hobbies.Add(v);
+            }
+
+            if (Leer)
+            {
+                string l = "Leer";
+                alguien.hobbies.Add(l);
+            }
+
+            if (Musica)
+            {
+                string m = "Escuchar musica";
+                alguien.hobbies.Add(m);
+            }
+
+            if (Gimnasio)
+            {
+                string g = "Ir al gimnasio";
+                alguien.hobbies.Add(g);
+            }
+
+            if (Persona.IsValidEmail(correo))
+            {
+                return View("Datos", alguien);
+            }
+            else
+            {
+                ViewBag.ErrorPerez = "Dirección de correo electrónica no válida";
+                return View();
+            }
+            
+        }
+
+        public ActionResult Datos()
+        {
+            return View();
+        }
+
     }
 }
---
+++ TareaHelpers/Models/Persona.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TareaHelpers.Models
+{
+    public class Persona
+    {
+
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        public string cedula { get; set; }
+
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        public string nombre { get; set; }
+
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        public string apellido { get; set; }
+
+        [Required (ErrorMessage ="Este campo es obligatorio")]
+        [Range (15,100, ErrorMessage = "Debe ser mayor de 15 años para registrarse")]
+        public int edad { get; set; }
+
+        public string telefono { get; set; }
+
+        public string estado_civil { get; set; }
+
+        public string correo { get; set; }
+
+        public List<string> hobbies = new List<string>();
+
+        public string genero { get; set; }
+
+        public static bool IsValidEmail(string email)
+        {
+            try
+            {
+                var addr = new System.Net.Mail.MailAddress(email);
+                return addr.Address == email;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+
+
+
+
+
+
+    }
+}
